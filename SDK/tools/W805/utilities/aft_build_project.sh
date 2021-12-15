890e193d53d7f0490019cb5e310862800d7f676b
Daniel Kucera <daniel.kucera@gmail.com>
2021-12-15 19:37:21 +0100

    use wine to wm_tool

--- SDK/tools/W805/utilities/aft_build_project.sh
+++ SDK/tools/W805/utilities/aft_build_project.sh
@@ -46,9 +46,9 @@ if [ $code_encrypt -eq 1 ]
   openssl rsautl -encrypt -in ../../../../../tools/W805/ca/key.txt -inkey ../../../../../tools/W805/ca/capub_"$prikey_sel".pem -pubin -out key_en.dat
   cat "$ProjName"_enc.bin key_en.dat > "$ProjName"_enc_key.bin
   cat "$ProjName"_enc_key.bin ../../../../../tools/W805/ca/capub_"$prikey_sel"_N.dat > "$ProjName"_enc_key_N.bin  
-  ../../../../../../../tools/W805/wm_tool.exe -b ./"$ProjName"_enc_key_N.bin -o ./"$ProjName" -it $img_type -fc 0 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
+  wine ../../../../../../../tools/W805/wm_tool.exe -b ./"$ProjName"_enc_key_N.bin -o ./"$ProjName" -it $img_type -fc 0 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
  else
-  ../../../../../../../tools/W805/wm_tool.exe -b ./"$ProjName".bin -o ./"$ProjName" -it $img_type -fc 0 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
+  wine ../../../../../../../tools/W805/wm_tool.exe -b ./"$ProjName".bin -o ./"$ProjName" -it $img_type -fc 0 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
 fi
 
 mkdir -p ../../../../../../../bin/W805
@@ -61,13 +61,13 @@ if [ $signature -eq 1 ]
   mv ./"$ProjName"_sign.img ../../../../../../../bin/W805/"$ProjName"_sign.img
 
   #when you change run-area image's ih, you must remake secboot img with secboot img's -nh address same as run-area image's ih
-  ../../../../../../../tools/W805/wm_tool.exe -b ../../../../../../../tools/W805/W805_secboot.bin -o ../../../../../../../tools/W805/W805_secboot -it 0 -fc 0 -ra $sec_img_pos -ih $sec_img_header -ua $upd_img_pos -nh $run_img_header  -un 0    
+  wine ../../../../../../../tools/W805/wm_tool.exe -b ../../../../../../../tools/W805/W805_secboot.bin -o ../../../../../../../tools/W805/W805_secboot -it 0 -fc 0 -ra $sec_img_pos -ih $sec_img_header -ua $upd_img_pos -nh $run_img_header  -un 0    
   cat ../../../../../../../tools/W805/"$ProjName"_secboot.img ../../../../../../../bin/W805/"$ProjName"_sign.img > ../../../../../../../bin/W805/"$ProjName".fls  
  else
   mv ./"$ProjName".img ../../../../../../../bin/W805/"$ProjName".img
   
   #when you change run-area image's ih, you must remake secboot img with secboot img's -nh address same as run-area image's ih
-  ../../../../../../../tools/W805/wm_tool.exe -b ../../../../../../../tools/W805/W805_secboot.bin -o ../../../../../../../tools/W805/W805_secboot -it 0 -fc 0 -ra $sec_img_pos -ih $sec_img_header -ua $upd_img_pos -nh $run_img_header  -un 0  
+  wine ../../../../../../../tools/W805/wm_tool.exe -b ../../../../../../../tools/W805/W805_secboot.bin -o ../../../../../../../tools/W805/W805_secboot -it 0 -fc 0 -ra $sec_img_pos -ih $sec_img_header -ua $upd_img_pos -nh $run_img_header  -un 0  
   cat ../../../../../../../tools/W805/"$ProjName"_secboot.img ../../../../../../../bin/W805/"$ProjName".img > ../../../../../../../bin/W805/"$ProjName".fls
 fi
 
@@ -76,10 +76,10 @@ if [ $zip_type -eq 1 ]
  then
   if [ $signature -eq 1 ]
    then
-    ../../../../../../../tools/W805/wm_tool.exe -b ./"$ProjName"_sign.img -o ../../../../../../../bin/W805/"$ProjName"_sign -it $img_type -fc 1 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
+    wine ../../../../../../../tools/W805/wm_tool.exe -b ./"$ProjName"_sign.img -o ../../../../../../../bin/W805/"$ProjName"_sign -it $img_type -fc 1 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
     mv ../../../../../../../bin/W805/"$ProjName"_sign_gz.img ../../../../../../../bin/W805/"$ProjName"_sign_ota.img
   else
-   ../../../../../../../tools/W805/wm_tool.exe -b ../../../../../../../bin/W805/"$ProjName".img -o ../../../../../../../bin/W805/"$ProjName" -it $img_type -fc 1 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
+   wine ../../../../../../../tools/W805/wm_tool.exe -b ../../../../../../../bin/W805/"$ProjName".img -o ../../../../../../../bin/W805/"$ProjName" -it $img_type -fc 1 -ra $run_img_pos -ih $run_img_header -ua $upd_img_pos -nh 0  -un 0
    mv ../../../../../../../bin/W805/"$ProjName"_gz.img ../../../../../../../bin/W805/"$ProjName"_ota.img
   fi
 fi
